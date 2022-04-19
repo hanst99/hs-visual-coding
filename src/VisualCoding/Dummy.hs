95b23c0973a5b6f3b86fa9695f8ba77ca7292dda
Hannes Steffenhagen <cubicentertain@gmail.com>
2022-04-19 13:41:31 +0200

    Add doctest

---
+++ src/VisualCoding/Dummy.hs
@@ -0,0 +1,11 @@
+-- | Dummy module so the tests have something to test
+module VisualCoding.Dummy
+  ( fibs )
+where
+
+-- | The fibonacci numbers
+--
+-- >>> take 10 fibs
+-- [1,1,2,3,5,8,13,21,34,55]
+fibs :: [Int]
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
